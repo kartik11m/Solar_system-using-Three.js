f356f635f030680318e934f5064927a1ed569d9c
kartik11m <kartikmaheshwari698@gmail.com>
2025-08-19 01:05:35 +0530

    adding resize eventListener so that it can fit into every screen size automatically if it gets resized

--- src/script.js
+++ src/script.js
@@ -98,9 +98,13 @@ const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
 
 const renderloop = () => {
-  
   renderer.render(scene, camera);
   window.requestAnimationFrame(renderloop);
 };
