5673507e3529527ae255dc95be744f981fbe24d2
kartik11m <kartikmaheshwari698@gmail.com>
2025-08-19 00:09:36 +0530

    adding basic setup like using sphere geometry,camera, material and adding them to the scene and also using renderloop so that it can updateautomatically

--- src/script.js
+++ src/script.js
@@ -1,1 +1,43 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { Pane } from "tweakpane";
 
+const pane = new Pane();
+
+const scene = new THREE.Scene();
+
+
+const sphereGeometry = new THREE.SphereGeometry(1,32,32);
+const sunMaterial = new THREE.MeshBasicMaterial(
+  {
+    color: "orange"
+  }
+);
+
+const sun = new THREE.Mesh(sphereGeometry,sunMaterial);
+sun.scale.setScalar(5);
+scene.add(sun);
+
+const camera = new THREE.PerspectiveCamera(
+  35,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  400
+);
+camera.position.z = 100;
+camera.position.y = 5;
+
+const canvas = document.querySelector("canvas.threejs");
+const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+
+const renderloop = () => {
+  
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(renderloop);
+};
+
+
+renderloop();
